71a281ec84ed8cbc1f764fb7d30f43febdb5cc46
Alex Novak <alex@alexnovak.dev>
2022-04-09 21:52:36 -0400

    Finished chapter 4

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,20 @@
+use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
-use crate::routes::{health_check, subscribe};
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+    let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Register our postgres connection as part of application state.
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
